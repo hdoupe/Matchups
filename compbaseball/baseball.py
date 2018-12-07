2f2f81d265d072a97a91b1db30251c8fa7fd229c
hdoupe <henrymdoupe@gmail.com>
2018-12-07 16:04:47 -0500

    tie up loose ends; fix some bugs

--- compbaseball/baseball.py
+++ compbaseball/baseball.py
@@ -9,47 +9,64 @@ def get_choices():
     with open(os.path.join(CURRENT_PATH, "playerchoices.json")) as f:
         return json.loads(f.read())
 
-def get_data(pitcher, start_date, end_date):
-    first_name, last_name = pitcher.split(" ")
+
+def get_inputs():
+    with open(os.path.join(CURRENT_PATH, "inputs.json")) as f:
+        return {"inputs": json.loads(f.read())}
+
+
+def pdf_to_clean_html(pdf):
+    """Takes a PDF and returns an HTML table without any deprecated tags or
+    irrelevant styling"""
+    return (pdf.to_html()
+            .replace(' border="1"', '')
+            .replace(' style="text-align: right;"', ''))
+
+
+def get_data(**kwargs):
+    defaults = get_inputs()
+    specs = {}
+    for param in defaults["inputs"]:
+        if kwargs.get(param, None) is not None:
+            specs[param] = kwargs[param][0] # comp params are lists right now.
+        else:
+            specs[param] = defaults["inputs"][param]["value"]
+    print(specs)
+    first_name, last_name = specs["pitcher"].split(" ")
     info = playerid_lookup(last_name, first_name)
     results = {'outputs': [], 'aggr_outputs': [], 'meta': {"task_times": [0]}}
     if len(info) == 0:
         return results
     mlbid = info.key_mlbam.values[0]
-    data = statcast_pitcher(start_date, end_date, 477132)
+    data = statcast_pitcher(specs["start_date"], specs["end_date"], mlbid)
     mean_stat = data.groupby(by="pitch_type")["release_speed", "launch_speed"].mean()
-    std_stat = data.groupby(by="pitch_type")["release_speed", "launch_speed"].mean()
+    std_stat = data.groupby(by="pitch_type")["release_speed", "launch_speed"].std()
 
     results['aggr_outputs'].append({
         'tags': {'statistic': 'mean'},
         'title': 'StatCast Means',
         'downloadable': [{'filename': 'mean' + '.csv',
                           'text': mean_stat.to_csv()}],
-        'renderable': mean_stat.to_html()})
+        'renderable': pdf_to_clean_html(mean_stat)})
     results['aggr_outputs'].append({
         'tags': {'statistic': 'std'},
         'title': 'StatCast Standard Dev.',
         'downloadable': [{'filename': 'std' + '.csv',
                           'text': std_stat.to_csv()}],
-        'renderable': std_stat.to_html()})
+        'renderable': pdf_to_clean_html(std_stat)})
     return results
 
 
 def validate_inputs(inputs):
     # date parameters alredy evaluated by webapp.
-    ew = {'errors': {}, 'warnings': {}}
-    pitcher = inputs["pitcher"]
+    inputs = inputs["inputs"]
+    ew = {"inputs": {'errors': {}, 'warnings': {}}}
+    pitcher = inputs["pitcher"][0]
     choices = get_choices()
     if pitcher not in choices["choices"]:
-        ew["errors"] = {"pitcher": f"pitcher {pitcher} not allowed"}
+        ew["inputs"]["errors"] = {"pitcher": f"pitcher {pitcher} not allowed"}
     return ew
 
 def parse_inputs(inputs):
     ew = validate_inputs(inputs)
-    return {"params": inputs, "jsonstrs": {"inputs": json.dumps(inputs)},
-            "errors_warnings": ew}
-
-
-def get_inputs():
-    with open(os.path.join(CURRENT_PATH, "inputs.json")) as f:
-        return {"inputs": json.loads(f.read())}
+    return (inputs, {"inputs": json.dumps(inputs)}, ew)
