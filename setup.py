1d5bfc4d421a3f3d9725b3680465c8e16acaa1ba
hdoupe <henrymdoupe@gmail.com>
2018-12-18 15:42:56 -0500

    Grab the TAG from environment variables

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 import setuptools
+import os
 
 with open("README.md", "r") as f:
     long_description = f.read()
 
 setuptools.setup(
     name="compbaseball",
-    version="0.1.4",
+    version=os.environ.get("TAG"),
     author="Hank Doupe",
     author_email="henrymdoupe@gmail.com",
     description="Documents COMP via baseball data examples.",
