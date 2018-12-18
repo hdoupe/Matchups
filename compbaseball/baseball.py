ddb80e61a0d597c5e4c5bea69f26bb0fd350f4ac
hdoupe <henrymdoupe@gmail.com>
2018-12-18 11:49:24 -0500

    Accept multiple batters

--- compbaseball/baseball.py
+++ compbaseball/baseball.py
@@ -45,33 +45,16 @@ def get_matchup(use_2018, user_mods):
     sc = scall.loc[(scall.date >= specs["start_date"]) & (scall.date < specs["end_date"])]
     del scall
     print('filtered by date')
-    pitcher, batter = specs["pitcher"], specs["batter"]
-    gb = sc.loc[(sc["player_name"]==pitcher) & (sc["batter_name"]==batter), :].groupby(
-        ["balls", "strikes"])
-    pitch_outcome_normalized = pd.DataFrame(gb["type"].value_counts(normalize=True))
-    pitch_outcome = pd.DataFrame(gb["type"].value_counts())
-    del gb
-    print('groupby1')
-
-    gb = sc.loc[(sc["player_name"]==pitcher) & (sc["batter_name"]==batter), :].groupby(
-        ["balls", "strikes"])
-    pitch_type_normalized = pd.DataFrame(gb["pitch_type"].value_counts(normalize=True))
-    pitch_type = pd.DataFrame(gb["pitch_type"].value_counts())
-    del gb
-    print('groupby2')
 
     gb = sc.groupby(
         ["balls", "strikes"])
     agg_pitch_outcome_normalized = pd.DataFrame(gb["type"].value_counts(normalize=True))
     del gb
-    print('groupby3')
 
     gb = sc.groupby(
         ["balls", "strikes"])
     agg_pitch_type_normalized = pd.DataFrame(gb["pitch_type"].value_counts(normalize=True))
     del gb
-    del sc
-    print('groupby4')
 
     results['aggr_outputs'].append({
         'tags': {'attribute': 'pitch-outcome'},
@@ -86,41 +69,65 @@ def get_matchup(use_2018, user_mods):
                           'text': agg_pitch_type_normalized.to_csv()}],
         'renderable': pdf_to_clean_html(agg_pitch_type_normalized)})
 
-    results["outputs"] = [
-        {
-            "dimension": "",
-            "tags": {"attribute": "pitch-outcome", "count": "normalized"},
-            'title': f'Normalized pitch outcome by count for {pitcher} v. {batter}',
-            'downloadable': [{'filename': f"normalized_pitch_outcome_{pitcher}_{batter}.csv",
-                              "text": pitch_outcome_normalized.to_csv()}],
-            'renderable': pdf_to_clean_html(pitch_outcome_normalized)
-        },
-        {
-            "dimension": "",
-            "tags": {"attribute": "pitch-outcome", "count": "raw-count"},
-            'title': f'Pitch outcome by count for {pitcher} v. {batter}',
-            'downloadable': [{'filename': f"pitch_outcome_{pitcher}_{batter}.csv",
-                              "text": pitch_outcome.to_csv()}],
-            'renderable': pdf_to_clean_html(pitch_outcome)
-        },
-        {
-            "dimension": "",
-            "tags": {"attribute": "pitch-type", "count": "normalized"},
-            'title': f'Normalized pitch type by count for {pitcher} v. {batter}',
-            'downloadable': [{'filename': f"normalized_pitch_type_{pitcher}_{batter}.csv",
-                              "text": pitch_type_normalized.to_csv()}],
-            'renderable': pdf_to_clean_html(pitch_type_normalized)
-        },
-        {
-            "dimension": "",
-            "tags": {"attribute": "pitch-type", "count": "raw-count"},
-            'title': f'Pitch type by count for {pitcher} v. {batter}',
-            'downloadable': [{'filename': f"pitch_type{pitcher}_{batter}.csv",
-                            "text": pitch_type.to_csv()}],
-            'renderable': pdf_to_clean_html(pitch_type)
-        },
-    ]
 
+    pitcher, batters = specs["pitcher"], specs["batter"]
+    for batter in batters:
+        print(pitcher, batter)
+        pdf = sc.loc[(sc["player_name"]==pitcher) & (sc["batter_name"]==batter), :]
+        if len(pdf) == 0:
+            pitch_outcome_normalized = pd.DataFrame()
+            pitch_outcome = pd.DataFrame()
+            pitch_type_normalized = pd.DataFrame()
+            pitch_type = pd.DataFrame()
+        else:
+            gb = pdf.loc[(pdf["player_name"]==pitcher) & (pdf["batter_name"]==batter), :].groupby(
+                ["balls", "strikes"])
+            pitch_outcome_normalized = pd.DataFrame(gb["type"].value_counts(normalize=True))
+            pitch_outcome = pd.DataFrame(gb["type"].value_counts())
+            del gb
+
+            gb = pdf.loc[(pdf["player_name"]==pitcher) & (pdf["batter_name"]==batter), :].groupby(
+                ["balls", "strikes"])
+            pitch_type_normalized = pd.DataFrame(gb["pitch_type"].value_counts(normalize=True))
+            pitch_type = pd.DataFrame(gb["pitch_type"].value_counts())
+            del gb
+            del pdf
+
+        results["outputs"] += [
+            {
+                "dimension": batter,
+                "tags": {"attribute": "pitch-outcome", "count": "normalized"},
+                'title': f'Normalized pitch outcome by count for {pitcher} v. {batter}',
+                'downloadable': [{'filename': f"normalized_pitch_outcome_{pitcher}_{batter}.csv",
+                                "text": pitch_outcome_normalized.to_csv()}],
+                'renderable': pdf_to_clean_html(pitch_outcome_normalized)
+            },
+            {
+                "dimension": batter,
+                "tags": {"attribute": "pitch-outcome", "count": "raw-count"},
+                'title': f'Pitch outcome by count for {pitcher} v. {batter}',
+                'downloadable': [{'filename': f"pitch_outcome_{pitcher}_{batter}.csv",
+                                "text": pitch_outcome.to_csv()}],
+                'renderable': pdf_to_clean_html(pitch_outcome)
+            },
+            {
+                "dimension": batter,
+                "tags": {"attribute": "pitch-type", "count": "normalized"},
+                'title': f'Normalized pitch type by count for {pitcher} v. {batter}',
+                'downloadable': [{'filename': f"normalized_pitch_type_{pitcher}_{batter}.csv",
+                                "text": pitch_type_normalized.to_csv()}],
+                'renderable': pdf_to_clean_html(pitch_type_normalized)
+            },
+            {
+                "dimension": batter,
+                "tags": {"attribute": "pitch-type", "count": "raw-count"},
+                'title': f'Pitch type by count for {pitcher} v. {batter}',
+                'downloadable': [{'filename': f"pitch_type{pitcher}_{batter}.csv",
+                                "text": pitch_type.to_csv()}],
+                'renderable': pdf_to_clean_html(pitch_type)
+            },
+        ]
+    del sc
     return results
 
 
@@ -129,13 +136,13 @@ def validate_inputs(inputs):
     inputs = inputs["matchup"]
     ew = {"matchup": {'errors': {}, 'warnings': {}}}
     for pos in ["pitcher", "batter"]:
-        player = inputs.get(pos, None)
-        if player is None:
+        players = inputs.get(pos, None)
+        if players is None:
             continue
-        player = player[0]
-        choices = get_choices()
-        if player not in choices["choices"]:
-            ew["matchup"]["errors"] = {pos: f"player \"{player}\" not allowed"}
+        for player in players:
+            choices = get_choices()
+            if player not in choices["choices"]:
+                ew["matchup"]["errors"] = {pos: f"player \"{player}\" not allowed"}
     return ew
 
 def parse_inputs(use_2018, inputs):
