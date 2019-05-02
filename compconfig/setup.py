38ec73f6b48e7183b7bc477ab9c75baef0d401ba
hdoupe <henrymdoupe@gmail.com>
2019-05-02 15:19:29 -0400

    Update compconfig/setup.py

--- compconfig/setup.py
+++ compconfig/setup.py
@@ -1,13 +1,15 @@
-# setup.py helps py.test find your tests in comp/tests/test_functions.py
+"""
+setup.py is used to build a light-weight python package around your
+COMP code. Add a MANIFEST.in file to specify data files that should
+be included with this package. Read more here:
+https://docs.python.org/3.8/distutils/sourcedist.html#specifying-the-files-to-distribute
+"""
 
 import setuptools
 import os
 
 setuptools.setup(
     name="compconfig",
-    version=os.environ.get("VERSION", "0.0.0"),
-    author="Hank Doupe",
-    author_email="henrymdoupe@gmail.com",
     description="COMP configuration files.",
     url="https://github.com/comp-org/COMP-Developer-Toolkit",
     packages=setuptools.find_packages(),
