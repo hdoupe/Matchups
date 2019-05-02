07cc577978ebee5640dfa72f6a97820aae110bd9
hdoupe <henrymdoupe@gmail.com>
2019-05-02 16:28:41 -0400

    Include conda-forge channel

--- compconfig/install.sh
+++ compconfig/install.sh
@@ -1,2 +1,2 @@
-conda install pandas pyarrow bokeh "paramtools>=0.5.2" fastparquet python-snappy pip
+conda install -c conda-forge pandas pyarrow bokeh "paramtools>=0.5.2" fastparquet python-snappy pip
 pip install git+https://github.com/hdoupe/Matchups.git@master
