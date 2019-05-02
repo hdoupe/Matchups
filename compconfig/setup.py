52f0eaf3b5061a51bb70bb48b5096bca43b36517
hdoupe <henrymdoupe@gmail.com>
2019-05-02 14:57:48 -0400

    Add setup.py code

--- compconfig/setup.py
+++ compconfig/setup.py
@@ -1,1 +1,15 @@
 # setup.py helps py.test find your tests in comp/tests/test_functions.py
+
+import setuptools
+import os
+
+setuptools.setup(
+    name="compconfig",
+    version=os.environ.get("VERSION", "0.0.0"),
+    author="Hank Doupe",
+    author_email="henrymdoupe@gmail.com",
+    description="COMP configuration files.",
+    url="https://github.com/comp-org/COMP-Developer-Toolkit",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+)
