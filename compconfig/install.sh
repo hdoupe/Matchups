e821e10b319df51cdbbe6a4d2f3b3bb503ee791d
hdoupe <henrymdoupe@gmail.com>
2019-05-02 15:26:51 -0400

    update install.sh

--- compconfig/install.sh
+++ compconfig/install.sh
@@ -1,1 +1,2 @@
-# bash commands for installing your package
+conda install pandas pyarrow bokeh "paramtools>=0.5.2" fastparquet python-snappy pip
+pip install git+https://github.com/hdoupe/Matchups.git@master
