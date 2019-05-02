7274b4adbfe66f74d99ef1ae6586d91cf1fb3b08
hdoupe <henrymdoupe@gmail.com>
2019-05-02 18:25:48 -0400

    Remove debug statement

--- compconfig/install.sh
+++ compconfig/install.sh
@@ -1,3 +1,2 @@
 conda install -c conda-forge pandas pyarrow bokeh "paramtools>=0.5.2" fastparquet python-snappy pip
 pip install git+https://github.com/hdoupe/Matchups.git@master
-python -c "import matchups; print(matchups.__version__)"
