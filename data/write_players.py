1057cf6767e79a3c94cebc12eb4be42bf4a4f530
hdoupe <henrymdoupe@gmail.com>
2020-01-17 13:04:56 -0500

    sort players

--- data/write_players.py
+++ data/write_players.py
@@ -3,7 +3,7 @@ import pandas as pd
 people = pd.read_csv("people.csv")
 people = people.loc[people.mlb_played_last > 2009, :]
 all_players = pd.DataFrame.from_dict({
-    "players": (people.name_first + " " + people.name_last).dropna().unique()
+    "players": sorted((people.name_first + " " + people.name_last).dropna().unique())
 })
 
 all_players.to_parquet("../matchups/players.parquet", engine="fastparquet")
